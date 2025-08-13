3c8f70e514bd42526f2647afb2afbd07edeac592
Jonathan Sedrez <jonathansedrez@gmail.com>
2025-08-12 22:39:21 -0300

    feat: get todays date

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ import { Temporal } from "@js-temporal/polyfill";
 
 const now = Temporal.Now.zonedDateTimeISO();
 console.log("Current date & time:", now.toString());
+
+const today = Temporal.PlainDate.from("2025-08-12");
+console.log("Today:", today.toString(), today, typeof today);
+
+
